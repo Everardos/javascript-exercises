b274d578649ab1dd5fc51eae89732805c17a4deb
Joshua Adkisson <112263154+Everardos@users.noreply.github.com>
2024-01-31 22:46:19 -0500

    Complete exercise eight

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let output = 0;
+  array.forEach((number) => output += number);
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let output = 1;
+  array.forEach((number) => output *= number);
+  return output
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return (a ** b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  let output = 1;
+  for (let i = n; i > 0; i--) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
