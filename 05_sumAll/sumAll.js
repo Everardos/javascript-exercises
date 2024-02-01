a7cf6db010b92c0fad8af9546f4681131bae671d
Joshua Adkisson <112263154+Everardos@users.noreply.github.com>
2024-01-31 21:57:34 -0500

    Complete exercise five

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) return "ERROR";
+    let smallNum = num1;
+    let bigNum = num2;
+    if (num1 > num2) {
+        smallNum = num2;
+        bigNum = num1;
+    }
+    let output = 0
+    for (let i = smallNum; i <= bigNum; i++) {
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line
