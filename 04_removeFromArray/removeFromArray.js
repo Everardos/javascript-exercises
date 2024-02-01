8af52faabad6f5021579016f301ab70c44000849
Joshua Adkisson <112263154+Everardos@users.noreply.github.com>
2024-01-31 21:44:43 -0500

    Complete exercise four

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...otherArgs) {
+    const argumentsArray = Array.from(arguments).slice(1);
+    let newArray = []
+    array.forEach((item) => {
+        if (argumentsArray.includes(item) === false) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
